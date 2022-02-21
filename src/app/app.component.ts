54224967a2aa7f4392ed4b26be8a1580675b524c
vin <vin@Vinishs-MacBook-Pro.local>
2022-02-21 18:16:55 +0530

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  username = 'akshay27395';
+  users$:any;
+
+  constructor(private readonly http: HttpClient) {}
+
+  searchUser(): void {
+    console.log(this.username);
+    this.users$ = this.http
+      .get(`https://api.github.com/search/users?q=${this.username}`)
+      .pipe(map((res: any) => res.items));
+  }
+}
---
+++ src/app/service.ts
@@ -0,0 +1,10 @@
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+
+export class ServiceAPI {
+
+    
+    constructor(private http: HttpClient) { }
+
+}
+
